0b942c42f62acb0e8b06f6b036baff88d404cfed
Oleg Petrov <oleg.petrov.eth@gmail.com>
2024-02-18 13:11:34 -0500

    import from previous repo

---
+++ src/lib/Vm.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+interface Vm {
+    function addr(uint256) external returns (address);
+    function warp(uint256 x) external;
+    function roll(uint x) external;
+    function deal(address who, uint256 amount) external;
+    function prank(address from) external;
+    function startPrank(address from) external;
+    function stopPrank() external;
+    function expectRevert(bytes calldata) external;
+    function expectEmit(bool, bool, bool, bool) external;
+    function mockCall(address, bytes calldata, bytes calldata) external;
+    function clearMockedCalls() external;
+    function expectCall(address, bytes calldata) external;
+    function startBroadcast() external;
+    function stopBroadcast() external;
+}
