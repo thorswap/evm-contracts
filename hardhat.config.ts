49f553c62312c9823dce6bf6c31fbf01efb0becb
Oleg Petrov <oleg.petrov.eth@gmail.com>
2024-02-21 17:51:55 -0500

    add compiler versions

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,54 @@ import "@nomicfoundation/hardhat-toolbox";
 import "tsconfig-paths/register";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [{
+      version: "0.8.4",
+      settings: {
+        metadata: {
+          bytecodeHash: "none",
+        },
+        optimizer: {
+          enabled: true,
+          runs: 800,
+        },
+      },
+    }, {
+      version: "0.8.6",
+      settings: {
+        metadata: {
+          bytecodeHash: "none",
+        },
+        optimizer: {
+          enabled: true,
+          runs: 800,
+        },
+      },
+    }, {
+      version: "0.8.10",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 1000,
+        },
+      },
+    }, {
+      version: "0.8.17",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 1000,
+        },
+      },
+    }]
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./src/contracts",
+    tests: "./tests",
+  },
 };
 
 export default config;
