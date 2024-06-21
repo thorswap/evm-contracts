a3f4bcf1ef30230a82efa6309e0ace6bdf87ba1d
Oleg Petrov <oleg.petrov.eth@gmail.com>
2024-06-21 12:15:01 -0400

    add polygon

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,24 +11,27 @@ export const ARBISCAN_API_KEY = process.env.ARBISCAN_API_KEY || "";
 export const BASESCAN_API_KEY = process.env.BASESCAN_API_KEY || "";
 export const BSCSCAN_API_KEY = process.env.BSCSCAN_API_KEY || "";
 export const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+export const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "";
 
 export const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 export const CMC_API_KEY = process.env.CMC_API_KEY || "";
 
 // helpers
 export const HARDHAT_DEPLOYER_ADDRESS = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+export const E_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
 
-export const TTP_ARB = "0x8bAF33E755Ee29E5E37F370a11A0a889DaC5d5f7";
-export const TTP_AVAX = "0x69ba883Af416fF5501D54D5e27A1f497fBD97156";
-export const TTP_BASE = "0x5505BE604dFA8A1ad402A71f8A357fba47F9bf5a";
-export const TTP_BSC = "0x5505BE604dFA8A1ad402A71f8A357fba47F9bf5a";
-export const TTP_ETH = "0xF892Fef9dA200d9E84c9b0647ecFF0F34633aBe8";
+export const ARB_TTP = "0x8bAF33E755Ee29E5E37F370a11A0a889DaC5d5f7";
+export const AVAX_TTP = "0x69ba883Af416fF5501D54D5e27A1f497fBD97156";
+export const BASE_TTP = "0x5505BE604dFA8A1ad402A71f8A357fba47F9bf5a";
+export const BSC_TTP = "0x5505BE604dFA8A1ad402A71f8A357fba47F9bf5a";
+export const ETH_TTP = "0xF892Fef9dA200d9E84c9b0647ecFF0F34633aBe8";
+export const POL_TTP = "0x542f4FFb7EBBc194cCbFd72ea92199a7f77573d7";
 
 export const ARB_WETH = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1";
 export const AVAX_WAVAX = "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7";
 export const BASE_WETH = "0x4200000000000000000000000000000000000006";
 export const ETH_WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-export const E_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE";
+export const POL_WMATIC = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
 
 export const ETH_UNISWAP_V2_ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
 export const WOOFI_V2_ROUTER = "0x4c4AF8DBc524681930a27b2F1Af5bcC8062E6fB7"; // verified on arb, avax, base, bsc, eth, op, pol
@@ -62,17 +65,24 @@ export const ETH_CONFIG = {
   accounts: [TS_DEPLOYER_PK],
 };
 
+export const POLYGON_CONFIG = {
+  url: "https://rpc.ankr.com/polygon",
+  chainId: 137,
+  accounts: [TS_DEPLOYER_PK],
+};
+
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   etherscan: {
-    apiKey: BASESCAN_API_KEY,
+    apiKey: POLYGONSCAN_API_KEY,
   },
   networks: {
     hardhat: {
       enableTransientStorage: true,
       gasPrice: 74e9
     },
-    mainnet: BASE_CONFIG
+    mainnet: BASE_CONFIG,
+    matic: POLYGON_CONFIG,
   },
   gasReporter: {
     enabled: true,
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,15 +2,15 @@ import hre, { ethers } from "hardhat";
 
 import {
     TS_DEPLOYER_ADDRESS,
-    BASE_WETH,
-    TTP_BASE,
+    POL_WMATIC,
+    POL_TTP,
     FEE_RECIPIENT_ETH,
     HARDHAT_DEPLOYER_ADDRESS,
     WOOFI_V2_ROUTER
 } from "../hardhat.config";
 
 const CONTRACT_NAME = "TSAggregatorWoofi";
-const CONTRACT_ARGS = [TTP_BASE, BASE_WETH, WOOFI_V2_ROUTER];
+const CONTRACT_ARGS = [POL_TTP, POL_WMATIC, WOOFI_V2_ROUTER];
 
 // const CONTRACT_NAME = "TSAggregatorTokenTransferProxy";
 // const CONTRACT_ARGS: any[] = [];
