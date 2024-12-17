118afc0e1e5c18b6679d96271f3826cc1ba96b72
Oleg Petrov <oleg.petrov.eth@gmail.com>
2024-12-17 17:32:13 +0400

    CF aggregator V3

--- src/interfaces/IChainflipVault.sol
+++ src/interfaces/IChainflipVault.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 interface IChainflipVault {
     function xSwapNative(
         uint32 dstChain,
-        bytes memory dstAddress,
+        bytes memory dstAddress, // 32 bytes expected by chainflip
         uint32 dstToken,
         bytes calldata cfParameters
     ) external payable;
